2ce8d30ff6aaa20cbb3ef2cc941773c0cbf5f9ec
th3mike <maarcio_lacerda@hotmail.com>
2021-09-01 08:31:03 -0300

    inicio do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 150vh;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1 class="display-1">
+    Em Desenvolvimento
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+*{
+  top: 0;
+  bottom: 0;
+}
+</style>
